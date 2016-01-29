5b4017f425234409306e64218e7bfde8a1d69866
Estelle Paus <epaus@servpronet.com>
2016-01-29 11:35:59 -0600

    Initial Commit

---
+++ TouchIDDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchIDDemo
+//
+//  Created by Estelle Paus on 1/29/16.
+//  Copyright © 2016 Estelle Paus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchIDDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchIDDemo
+//
+//  Created by Estelle Paus on 1/29/16.
+//  Copyright © 2016 Estelle Paus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
